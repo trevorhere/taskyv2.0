905b5b24ed92b030391c405ddb7faf3787105cb0
trevorhere <trevoranthonylane@gmail.com>
2018-11-18 20:08:52 -0700

    homepage

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 import {css} from 'react-emotion';
 import background from "../assets/city.jpeg";
+import { BarLoader } from 'react-spinners';
+
+const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+`;
+
+// const componentStyle = css`
+//   height: 80vh;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   text-align: ;center;
+// `;
 
 const componentStyle={
   //background: "url("+ background +") no-repeat center center fixed",
-  background: "linear-gradient(rgba(20,20,20, .5), rgba(20,20,20, .5)),url( "+ background + ")",
+  background: "linear-gradient(rgba(20,20,20, .75), rgba(20,20,20, .75)),url( "+ background + ")",
   height: "93vh",
   backgroundSize: "cover",
   display: "flex",
@@ -34,6 +49,13 @@ class HomePage extends Component {
       <div style={componentStyle}>
         <div style={headerStyle}>
             <h3 style={titleStyle} >W E L C O M E</h3>
+                <BarLoader
+                className={override}
+                sizeUnit={"px"}
+                size={10}
+                color={'#F44336'}
+                loading={this.props.loading}
+              />
         </div>
       </div>
       );
