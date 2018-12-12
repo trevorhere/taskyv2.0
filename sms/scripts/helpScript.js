806e059b7736943ee1dcc5645749ef5751da34a1
trevorhere <trevoranthonylane@gmail.com>
2018-12-12 15:24:33 -0700

    fix delete list

--- sms/scripts/helpScript.js
+++ sms/scripts/helpScript.js
@@ -1,11 +1,10 @@
-let helpScript = ` Taskive: Tasks via SMS
-The following commands are available:
-1. \"?\": Show this message
-2. \"list(s)\": Show your lists (current list marked with *)
-3. \"item(s)\": View items in current lists
-4. \"select 'name' \": Select the list by 'name'
-5. \"add 'list' \": Adds 'list' as a new list
-6. \"remove\": Removes currently selected 'list'
+let helpScript = `
+Commands:
+1. \"?\": view commands
+2. \"list(s)\": Show your lists (currently selected list marked with *)
+3. [list name]: Show items in named list and select named list
+4. \"add 'list' \": Adds 'list' as a new list
+6. \"remove\": Removes currently selected list
 7. \"plus 'item' \": Adds 'item' as an item to currently selected list
 8. \"minus 'item' \": Removes 'item' from curently selected list
 `
